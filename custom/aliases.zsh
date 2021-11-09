c9000d4dd870cf3330916466ebabe28491763800
Deon Pearson <me@deonpearson.com>
2021-11-09 18:20:32 +0200

    alias changes

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -12,6 +12,9 @@ alias kc='kubectl'
 alias kcl='kubectl --context minikube'
 alias kcevict='kubectl --context docker-desktop get pod --all-namespaces --field-selector 'status.phase!=Running' -o json | kubectl delete -f -'
 #---------------------------------------------------------------------------------------------------
+alias ks='kubeseal'
+alias ksl='kubeseal --context minikube'
+#---------------------------------------------------------------------------------------------------
 alias sternl='stern --context minikube'
 #---------------------------------------------------------------------------------------------------
 alias helml='helm --kube-context minikube'
