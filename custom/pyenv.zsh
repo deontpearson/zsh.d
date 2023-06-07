22fc647a10dacecf401ac2bf4fb3b7a0d3c6e83d
Deon Pearson <46675621+deontpearson@users.noreply.github.com>
2023-06-07 08:27:39 +0200

    better pyenv create

--- custom/pyenv.zsh
+++ custom/pyenv.zsh
@@ -1,9 +1,3 @@
-export PYTHON_3_11_VERSION=3.11.1
-export PYTHON_3_10_VERSION=3.10.9
-export PYTHON_3_9_VERSION=3.9.16
-export PYTHON_3_8_VERSION=3.8.16
-export PYTHON_3_7_VERSION=3.7.16
-
 function pyenv-create() {
   usage="USAGE
   pyenv-create <arguments>
@@ -13,8 +7,7 @@ OPTIONAL ARGUMENTS
   -n name       the name of the virtual environment, default is the folder name
   -h            help about the command
 "
-
-  local python_version=$PYENV_DEFAULT_PYTHON
+  local python_version
   local env_name
 
   while getopts ":v:n:h" opt; do
@@ -25,12 +18,9 @@ OPTIONAL ARGUMENTS
     esac
   done
 
-  output=$(pyenv versions | grep -c $python_version)
-
-  if [ "$output" == 0 ]; then
-    echo "installing $python_version"
-    pyenv install $python_version
-  fi
+  python_latest=$(pyenv install --list | awk '{ print $1 }' | grep ^$PYTHON_VERSION | sort -V | tail -n 1)
+  echo "installing $python_latest"
+  pyenv install $python_latest --skip-existing
 
   if [ ! -n "$env_name" ]; then
     echo $usage && return 1
@@ -38,7 +28,5 @@ OPTIONAL ARGUMENTS
 
   pyenv virtualenv $python_version $env_name
   pyenv activate $env_name
-  # $(pyenv root)/versions/$env_name/bin/python -m pip install --upgrade pip
-  # $(pyenv root)/versions/$env_name/bin/python -m pip install pip-tools
   pyenv local $env_name
 }
